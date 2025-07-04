1cf0fa1380f86eede15a7e4bb68585427c66f5b1
Philips <philipsjose2003@gmail.com>
2025-07-04 16:47:23 +0530

    Used capture lists to prevent strong reference cycles

--- ChatApp/ViewControllers/CreateAccountViewController.swift
+++ ChatApp/ViewControllers/CreateAccountViewController.swift
@@ -103,15 +103,16 @@ class CreateAccountViewController: UIViewController {
             return
         }
         showLoadingView()
-        checkIfExists(username: username) { usernameExists in
+        checkIfExists(username: username) { [weak self] usernameExists in
+            guard let strongSelf =  self else { return }
             if !usernameExists{
-                self.createUser(username: username, email: email, password: password) { result, error in
+                strongSelf.createUser(username: username, email: email, password: password) { result, error in
                     if let error = error {
-                        self.presentErrorAlert(title: "Create Account Failed", message: error)
+                        strongSelf.presentErrorAlert(title: "Create Account Failed", message: error)
                         return
                     }
                     guard let result = result else {
-                        self.presentErrorAlert(title: "Create Account Failed", message: "Please try again later.")
+                        strongSelf.presentErrorAlert(title: "Create Account Failed", message: "Please try again later.")
                         return
                     }
                     let userId = result.user.uid
@@ -136,8 +137,8 @@ class CreateAccountViewController: UIViewController {
                 
             }
             else {
-                self.presentErrorAlert(title: "Username In Use", message: "Please try a different username.")
-                self.removeLoadingView()
+                strongSelf.presentErrorAlert(title: "Username In Use", message: "Please try a different username.")
+                strongSelf.removeLoadingView()
             }
         }
         
@@ -155,8 +156,9 @@ class CreateAccountViewController: UIViewController {
     }
     
     func createUser(username: String, email: String, password: String, completion: @escaping (_ result: AuthDataResult?, _ error: String?) -> Void ) {
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
-            self.removeLoadingView()
+        Auth.auth().createUser(withEmail: email, password: password) { [weak self]result, error in
+            guard let strongSelf = self else { return }
+            strongSelf.removeLoadingView()
             if let error = error {
                 print(error.localizedDescription)
                 var errorMessage = "Something went wrong. Please try again later."
